fadf5a296295e2734deefedb63ecd40aafdee63c
CounKite <43300000+CounKite@users.noreply.github.com>
2018-10-05 18:47:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const bot = new Discord.Client();
 bot.on("ready", async () => {
     console.log(`${bot.user.username} ${bot.guilds.size} sunucuda aktif!`);
   
-    bot.user.setActivity("Gatinciyi", {type: "PLAYING"});
+    bot.user.setActivity("Grand Theft Auto V", {type: "PLAYING"});
   });
   
                                                                                                                                  
-bot.login('NDk3NDgzMzAwOTg2OTQ1NTQ2.Dpf1YQ.gsrIVxJeEbu64rBAYb4XCJpkoX8');
+bot.login(process.env.bot_TOKEN);
 
